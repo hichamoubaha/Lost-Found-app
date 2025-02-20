c71f1a05d592c59dc76e0f5a7523f0da2510c43e
hichamoubaha <oubahahicham7@gmail.com>
2025-02-20 04:53:44 +0000

    modify the routes for authenfification

--- routes/web.php
+++ routes/web.php
@@ -38,10 +38,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('annonces', AnnonceController::class)->except(['index', 'show']);
 });
+
 
 require __DIR__.'/auth.php';
